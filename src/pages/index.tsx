127a87eb82fd8182191f8d60a52231e0056a32b6
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-09-23 12:12:30 -0300

    personalização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,16 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          DevTips
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+            materiais e referências
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            veja mais
           </Link>
         </div>
       </div>
