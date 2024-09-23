b082dbf91048ed2f63e740f6a94e585c4e3545d6
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-09-23 12:58:40 -0300

    topicos basicos

--- docs/roadmaps/filosofia.md
+++ docs/roadmaps/filosofia.md
@@ -2,7 +2,7 @@
 sidebar_position: 6
 ---
 
-# Apreço pela vida e o conhecimento
+# Estudos - apreço pela vida e o conhecimento
 
 ### Material de estudo
 
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,10 +35,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -46,11 +42,6 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -79,11 +70,6 @@ const config: Config = {
           label: 'Tutorial',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
